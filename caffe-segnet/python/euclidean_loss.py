b11b79a91e4c5e4fdb202482d3393fefcda5a143
faraxnak <farid@Farids-MacBook-Pro.local>
2017-05-22 19:47:24 +0430

    euc loss correction 2

--- caffe-segnet/python/euclidean_loss.py
+++ caffe-segnet/python/euclidean_loss.py
@@ -7,6 +7,7 @@ class EuclideanLossLayer(caffe.Layer):
     Compute the Euclidean Loss in the same manner as the C++ EuclideanLossLayer
     to demonstrate the class interface for developing layers in Python.
     """
+    result = None
 
     def setup(self, bottom, top):
         # check input pair
@@ -15,16 +16,30 @@ class EuclideanLossLayer(caffe.Layer):
 
     def reshape(self, bottom, top):
         # check input dimensions match
-        if bottom[0].count != bottom[1].count:
+        self.result = np.squeeze(np.argmax(bottom[0].data[...],axis=1))
+        print(result.shape)
+        for i in range(0,bottom[0].data.shape[0]):
+            estimate = np.squeeze(bottom[0].data[i,:])
+            ind = np.argmax(estimate, axis=0)
+            for j in range(0,estimate.shape[0]):
+                result[i, ind == j] = j
+        print(result.shape)
+        if bottom[0].count != result.count:
             raise Exception("Inputs must have the same dimension.")
         # difference is shape of inputs
-        self.diff = np.zeros_like(bottom[0].data, dtype=np.float32)
+        self.diff = np.zeros_like(bottom[1].data, dtype=np.float32)
         # loss output is scalar
         top[0].reshape(1)
 
     def forward(self, bottom, top):
-        self.diff[...] = bottom[0].data - bottom[1].data
+        self.diff[...] = self.result.data - bottom[1].data
         top[0].data[...] = np.sum(self.diff**2) / bottom[0].num / 2.
+        # loss = np.zeros(bottom[0].data.shape[0], dtype=np.float32)
+        # for i in range(0,bottom[0].data.shape[0]):
+        #     self.diff[i,:] = bottom[0].data - bottom[1].data
+        #     loss[i] = np.sum(self.diff[i,:]**2) / bottom[i,0].num / 2.
+        #     # top[0].data[...] = np.sum(self.diff**2) / bottom[0].num / 2.
+        # top[0].data[0] = np.sum(loss)
 
     def backward(self, top, propagate_down, bottom):
         for i in range(2):
