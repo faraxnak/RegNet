c9ab893325f6860da220597ebc326862bfb9d56e
faraxnak <farid@Farids-MacBook-Pro.local>
2017-05-22 19:51:34 +0430

    euc loss correction 4

--- caffe-segnet/python/euclidean_loss.py
+++ caffe-segnet/python/euclidean_loss.py
@@ -17,14 +17,14 @@ class EuclideanLossLayer(caffe.Layer):
     def reshape(self, bottom, top):
         # check input dimensions match
         self.result = np.zeros_like(bottom[1].data, dtype=np.float32)
-        print(result.shape)
+        print(self.result.shape)
         for i in range(0,bottom[0].data.shape[0]):
             estimate = np.squeeze(bottom[0].data[i,:])
             ind = np.argmax(estimate, axis=0)
             for j in range(0,estimate.shape[0]):
                 result[i, ind == j] = j
-        print(result.shape)
-        if bottom[0].count != result.count:
+        print(self.result.shape)
+        if bottom[0].count != self.result.count:
             raise Exception("Inputs must have the same dimension.")
         # difference is shape of inputs
         self.diff = np.zeros_like(bottom[1].data, dtype=np.float32)
