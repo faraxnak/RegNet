3deb4754b5479ce166fb3b2977d30874668e5858
faraxnak <farid@Farids-MacBook-Pro.local>
2017-05-27 17:39:38 +0430

    edited loss formula

--- caffe-segnet/python/euclidean_loss.py
+++ caffe-segnet/python/euclidean_loss.py
@@ -36,9 +36,10 @@ class EuclideanLossLayer(caffe.Layer):
     def forward(self, bottom, top):
         diff = self.result - bottom[1].data
         print(self.index.shape)
+        print(self.diff)
         sys.stdin.read(1)
         # self.diff[self.index] = diff
-        self.diff[np.array(self.index.data, dtype=np.uint16)] = diff
+        # self.diff[np.array(self.index.data, dtype=np.uint16)] = diff
         top[0].data[...] = np.sum(diff**2) / bottom[0].num / 2.
         # loss = np.zeros(bottom[0].data.shape[0], dtype=np.float32)
         # for i in range(0,bottom[0].data.shape[0]):
