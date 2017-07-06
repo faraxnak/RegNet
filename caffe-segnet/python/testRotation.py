ccdbe7e38f713c995fdc6bb7200f904c6784fc3a
faraxnak <farid@Farids-MacBook-Pro.local>
2017-07-06 14:51:01 +0430

    changed test file for rotation

--- caffe-segnet/python/testRotation.py
+++ caffe-segnet/python/testRotation.py
@@ -38,7 +38,7 @@ class TestRotation(caffe.Layer):
             # estimateImg = np.squeeze(estimate)
             # print(estimate.shape)
             label = bottom[3].data[i,:]
-            print('rotation in degrees:', label))
+            print('rotation in degrees:', label)
             print('estimate in degrees: ', estimate[0])
             rotated = imutils.rotate(image, estimate[0])
             # print(estimateImg.max(axis=1))
